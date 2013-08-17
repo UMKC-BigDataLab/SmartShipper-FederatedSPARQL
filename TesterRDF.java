470bb0222d65051624d0d256fd8737e8c1eb5c4c
vptarmigan <vicky.ptarmigan@gmail.com>
2013-08-17 12:54:44 -0500

    init

---
+++ TesterRDF.java
@@ -0,0 +1,21 @@
+/*
+ * 8/16/13 Friday
+ * Goal: Get data set from file. Execute a query on dataset.
+ */
+
+package rdf;
+
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.vocabulary.*;
+
+import java.io.*;
+
+public class TesterRDF {
+	public static void main(String arg[])	{
+		
+		System.out.println("Hello World!");
+
+		String fileName = "data/thirteenDrugs.nt";
+		File file = new File(fileName);
+	}
+}
