a6ebda61c1d23f96b3e5228e57b301dd4b58d702
vptarmigan <vicky.ptarmigan@gmail.com>
2013-08-17 16:17:12 -0500

    finished little prog with read in dataset

--- TesterRDF.java
+++ TesterRDF.java
@@ -7,15 +7,44 @@ package rdf;
 
 import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.vocabulary.*;
+import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.shared.JenaException;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.Syntax;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
 
 import java.io.*;
 
 public class TesterRDF {
 	public static void main(String arg[])	{
 		
-		System.out.println("Hello World!");
 
-		String fileName = "data/thirteenDrugs.nt";
+		String fileName = "src/rdf/data/thirteenDrugs.nt";
+		String selectQuery = "select * where {?s ?p ?o}";
+
 		File file = new File(fileName);
+		if(file.exists())	{
+			Model model = null;
+			try	{
+				model = FileManager.get().loadModel(file.getCanonicalPath());
+				Query query = QueryFactory.create(selectQuery, Syntax.syntaxSPARQL_11);
+				QueryExecution qexec = QueryExecutionFactory.create(query, model);
+				ResultSet results = qexec.execSelect();
+				System.out.println(ResultSetFormatter.asText(results));
+			}
+			catch (JenaException e)	{
+				System.out.println("Invalid syntax");
+			}
+			catch (IOException e)	{
+				e.printStackTrace();
+			}
+		}
+		else	{
+			System.out.println(fileName + " does not exist.");	
+		}
 	}
 }
