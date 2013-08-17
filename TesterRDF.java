71ddbd017ff71657f1cde715bcef8c8e7658ff57
vptarmigan <vicky.ptarmigan@gmail.com>
2013-08-17 17:04:10 -0500

    using sparqlService

--- TesterRDF.java
+++ TesterRDF.java
@@ -22,7 +22,36 @@ import java.io.*;
 public class TesterRDF {
 	public static void main(String arg[])	{
 		
+		askRemote();
+		//modelFromFile();
+		
+	}
 
+	static void askRemote()	{
+	
+		//how about... getting dataset from somewhere else?
+		//I want to know how many birds there are that eat meat.
+		//Or how about just mammals.  - How to ask a query to a sparql endpoint?
+		String selectQuery = 
+			"PREFIX dbpedia: <http://dbpedia.org/resource/>"
+			+ "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>"
+			+ "select distinct ?thing where {?thing dbpedia-owl:class dbpedia:Bird } LIMIT 100";
+		Model model = null;
+		try	{
+			Query query = QueryFactory.create(selectQuery, Syntax.syntaxSPARQL_11);
+			QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+			ResultSet results = qexec.execSelect();
+			System.out.println(ResultSetFormatter.asText(results));
+		}
+		catch (JenaException e)	{
+			System.out.println("Invalid syntax");
+			e.printStackTrace();
+		}
+	
+	}
+
+	static void modelFromFile()	{
+		//just from file
 		String fileName = "src/rdf/data/thirteenDrugs.nt";
 		String selectQuery = "select * where {?s ?p ?o}";
 
